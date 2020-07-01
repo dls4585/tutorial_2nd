0e0e104bbae3c8c3300f99525aba0ea90373d369
dls4585 <dls4585@naver.com>
2020-07-01 14:37:39 +0900

    Modified templates to display posts from database

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     #127.0.0.1:8000/ 으로 들어오면 view.post_list를 보여줘
+    #기본화면
     path('', views.post_list, name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    #데이터베이스에서 Post의 객체들을 불러와서 posts에 저장
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    #posts 라는 문자열이 나오면 posts인거다?
 
