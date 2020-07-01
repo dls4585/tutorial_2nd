5780786d763a85ad9c566346e79fe1a53bac367f
dls4585 <dls4585@naver.com>
2020-07-01 14:16:17 +0900

    Changed the HTML for the site

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #127.0.0.1:8000/ 으로 들어오면 view.post_list를 보여줘
+    path('', views.post_list, name='post_list'),
+]
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    # blog 애플리케이션에서 메인 urls.py로 url을 가져오기
 ]
